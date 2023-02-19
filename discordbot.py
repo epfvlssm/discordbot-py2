6fc727e939500a9e9959da481dd4a0eb1427cce2
epfvlssm <124798971+epfvlssm@users.noreply.github.com>
2023-02-20 02:59:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,11 @@ from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 import os
+from discord import Intents
+from discord.ext import commands
+from discord import Game
+from discord import Status
+from discord import Object
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
@@ -18,14 +23,43 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    # else:
+    #     await message.channel.send('callback: ' + message.content)
 
-    if message.content == f'{PREFIX}call':
+    if message.content == '!call':
         await message.channel.send("callback!")
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix='!',
+            intents=Intents.all(),
+            sync_command=True,
+            application_id=1073863485999550474
+        )
+        self.initial_extension = [
+            "Cogs.hello",
+            "Cogs.button",
+            "Cogs.select_1",
+            "Cogs.check",
+            "Cogs.game"
+        ]
+
+    async def setup_hook(self):
+        for ext in self.initial_extension:
+            await self.load_extension(ext)
+        await bot.tree.sync(guild=Object(id=1072809889329594430))
+
+    async def on_ready(self):
+        print("login")
+        print(self.user.name)
+        print(self.user.id)
+        print("===============")
+        game = Game("....")
+        await self.change_presence(status=Status.online, activity=game)
 
 
+bot = MyBot()
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
